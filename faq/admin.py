7c3040c071aa4d00279f527a2ced5986c6e44572
davel1 <dvl4ht@gmail.com>
2015-06-13 15:44:26 +0300

    Изменена структура вопросов и ответов. Переработан интерфейс админимтратора

--- faq/admin.py
+++ faq/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from models import quote
+from models import question, answer
 
 # Register your models here.
 
-admin.site.register(quote)
+class AnswerInline(admin.StackedInline):
+    model = answer
+    extra = 1
+    
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('q', 'q_date')
+    fields = ['q', 'q_date']
+    inlines = [AnswerInline]
+
+admin.site.register(question, QuestionAdmin)
--- faq/models.py
+++ faq/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-class quote(models.Model):
+class question(models.Model):
     q = models.TextField()
     q_date = models.DateTimeField('data quote')
+    
+class answer(models.Model):
     r = models.TextField()
     r_date = models.DateTimeField('data request')
     public = models.BooleanField()
-    
+    child = models.ForeignKey(question)
