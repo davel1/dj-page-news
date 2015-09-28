930b27b407e66d3db1a82e41f91a37e83d1f5b9b
davel1 <dvl4ht@gmail.com>
2015-09-28 11:06:23 +0300

    release 1.0

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
+virtualenv dj && cd ./dj
+source ./bin/activate
+pip install --upgrade pip
 pip install django
 pip install django-import-export
 pip install python-social-auth
 pip install django-wysiwyg
 pip install django-ckeditor
+pip install Pillow
+git clone https://github.com/davel1/dj-page-news
+rm -rf ./*migrations* 
 
 
